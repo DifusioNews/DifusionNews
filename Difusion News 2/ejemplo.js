4ab04b843cff10e919a93c5dd51b328a397b08ce
DifusioNews <137871617+DifusioNews@users.noreply.github.com>
2023-06-29 21:04:25 -0400

    Add files via upload

---
+++ Difusion News 2/ejemplo.js
@@ -0,0 +1,149 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var cardLink = document.querySelector('.card-link');
+    var videoDetail = document.querySelector('.video-detail');
+    var ir_home = document.querySelector('.ir_a_home1');
+    
+
+    cardLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      videoDetail.style.display = 'block';
+      videoDetail.classList.add('sweep-in');
+    });
+    ir_home.addEventListener('click', function(event) {
+      event.preventDefault();
+      videoDetail.style.display = 'none';
+    })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link2');
+  var videoDetail = document.querySelector('.video-detail2');
+  var ir_home = document.querySelector('.ir_a_home2');
+  
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link3');
+  var videoDetail = document.querySelector('.video-detail3');
+  var ir_home = document.querySelector('.ir_a_home3');
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link4');
+  var videoDetail = document.querySelector('.video-detail4');
+  var ir_home = document.querySelector('.ir_a_home4');
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link5');
+  var videoDetail = document.querySelector('.video-detail5');
+  var ir_home = document.querySelector('.ir_a_home5');
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link6');
+  var videoDetail = document.querySelector('.video-detail6');
+  var ir_home = document.querySelector('.ir_a_home6');
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link7');
+  var videoDetail = document.querySelector('.video-detail7');
+  var ir_home = document.querySelector('.ir_a_home7');
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link8');
+  var videoDetail = document.querySelector('.video-detail8');
+  var ir_home = document.querySelector('.ir_a_home8');
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var cardLink = document.querySelector('.card-link9');
+  var videoDetail = document.querySelector('.video-detail9');
+  var ir_home = document.querySelector('.ir_a_home9');
+
+  cardLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'block';
+    videoDetail.classList.add('sweep-in');
+  });
+  ir_home.addEventListener('click', function(event) {
+    event.preventDefault();
+    videoDetail.style.display = 'none';
+  })
+});
